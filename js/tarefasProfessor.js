5b8e75147c68b28bcdedc8ff6a91bac00541bd44
unknown <biancapeleao@gmail.com>
2023-06-09 12:40:59 -0300

    comitei

--- js/tarefasProfessor.js
+++ js/tarefasProfessor.js
@@ -13,33 +13,33 @@ buttonSairMNodal.addEventListener('click', () => {
     modalTarefaAdc.classList.add('d-none')
     modalTarefaAdc.classList.remove('d-flex')
 })
-var input = document.getElementById('imagem');
-var visualizacao = document.getElementById('visualizacao');
-var imgVisu = document.getElementById('imgVisu');
+// var input = document.getElementById('imagem');
+// var visualizacao = document.getElementById('visualizacao');
+// var imgVisu = document.getElementById('imgVisu');
 
-const exibirImagem = (event) => {
-    console.log('2');
-    var input = event.target;
-    var visualizacao = document.getElementById('visualizacao');
+// const exibirImagem = (event) => {
+//     console.log('2');
+//     var input = event.target;
+//     var visualizacao = document.getElementById('visualizacao');
 
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        console.log('3');
-        reader.onload = function (e) {
-            console.log('4');
-            imgVisu.src = e.target.result
-            //visualizacao.style.background = "url('" + e.target.result + "')";
-            //visualizacao.style.backgroundRepea = "no-repeat";
-            console.log(e.target.result);
-        };
+//     if (input.files && input.files[0]) {
+//         var reader = new FileReader();
+//         console.log('3');
+//         reader.onload = function (e) {
+//             console.log('4');
+//             imgVisu.src = e.target.result
+//             console.log(e.target.result);
+            
+//         };
 
-        reader.readAsDataURL(input.files[0]);
-    } else {
-        visualizacao.style.background = 'none';
-    }
-};
+//         reader.readAsDataURL(input.files[0]);
+//     } else {
+//         visualizacao.style.background = 'none';
+//     }
+    
+// };
 
-input.addEventListener('change', (event) => {
-    console.log('1');
-    exibirImagem(event);
-});
+// input.addEventListener('change', (event) => {
+//     console.log('1');
+//     exibirImagem(event);
+// });
