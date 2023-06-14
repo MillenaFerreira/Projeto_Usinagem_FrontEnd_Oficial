a95a2cddd48d03f8398c66100e9a1dfcbd03753e
Bianca Leão <22282176@CFP127ALUNO.LOCAL>
2023-06-14 16:40:55 -0300

    já pegando valor do critério

--- js/criteriosProfessor.js
+++ js/criteriosProfessor.js
@@ -11,6 +11,8 @@ const btnAdc = document.getElementById('adicionar')
 const modalAdc = document.getElementById('modal__adicionar__criterio')
 const sairCriterioModal = document.getElementById('sairCriterioModal')
 
+const sendCriterio = document.getElementById('sendCriterio')
+
 btnAdc.addEventListener('click', (event) => {
     event.preventDefault()
     modalAdc.classList.add('d-flex')
@@ -21,4 +23,37 @@ sairCriterioModal.addEventListener('click', (event) => {
     event.preventDefault()
     modalAdc.classList.add('d-none')
     modalAdc.classList.remove('d-flex')
+})
+
+const adicionarCriterio = () => {
+    
+    const descricaoCriterio = document.getElementById('descricaoCriterio')
+    const valorCritico = document.getElementById('critico')
+    const observacaoNota = document.getElementById('observacaoNota')
+
+    let valorCriticoStatus;
+    let observacaoNotaStatus;
+
+    if(valorCritico.checked){
+        valorCriticoStatus = true
+    }else{
+        valorCriticoStatus = false
+    }
+
+    if(observacaoNota.checked){
+        observacaoNotaStatus = true
+    }else{
+        observacaoNotaStatus = false
+    }
+
+    console.log(descricaoCriterio.value);
+    console.log(valorCriticoStatus);
+    console.log(observacaoNotaStatus);
+    
+    
+}
+
+sendCriterio.addEventListener('click', (event) => {
+    event.preventDefault()
+    adicionarCriterio()
 })
