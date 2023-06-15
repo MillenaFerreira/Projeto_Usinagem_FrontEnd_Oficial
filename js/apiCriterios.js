710d103f184029dfbe1bb5a455d624e3d5096210
Bianca Leão <22282176@CFP127ALUNO.LOCAL>
2023-06-15 14:35:50 -0300

    já está criando o valor

--- js/apiCriterios.js
+++ js/apiCriterios.js
@@ -6,12 +6,43 @@ export const pesquisarCriterios = async () => {
     const data = await response.json();
     return data.criterios;
   }
+
+export const pesquisarDesejadoPeloIdCriterio = async (idCriterio) => {
+    const url = `https://usinagem-senai-api.cyclic.app/v1/projeto-usinagem/resultado-desejado?idCriterio=${idCriterio}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.resultado_desejado;
+  }
+
+
+  export const createDesejado = async (desejado) => {
+    const url = `https://usinagem-senai-api.cyclic.app/v1/projeto-usinagem/resultado-desejado`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(desejado)
+    };
+    fetch(url, options)
+    .then(response => {
+      if (response.ok) {
+        location.reload();
+  
+      } else {
+        console.log('Erro ao criar o desejado.');
+      }
+    })
+    .catch(error => {
+      console.log('Ocorreu um erro na requisição:', error);
+    });
+  }
+
 export const pesquisarCriteriosPeloIdTarefa = async (idTarefa) => {
     const url = `https://usinagem-senai-api.cyclic.app/v1/projeto-usinagem/criterio?idTarefa=${idTarefa}`;
     const response = await fetch(url);
     const data = await response.json();
     return data.criterios;
-    
   }
 
   export const updateCriterio = async (idCriterio, bodyCriterio) => {
--- js/desejadoProfessor.js
+++ js/desejadoProfessor.js
@@ -1,3 +1,7 @@
+'use strict'
+
+import { pesquisarDesejadoPeloIdCriterio, createDesejado } from './apiCriterios.js';
+
 
 const buttonAdicionar = document.getElementById('adicionar')
 const modalCriterioAdc = document.getElementById('modal__adicionar__criterio')
@@ -7,6 +11,10 @@ const idDoCriterio = localStorage.getItem('idCriterio')
 const titleCriterio = document.querySelector('.titleCriterio')
 
 titleCriterio.textContent = 'Resultado desejado do critério: ' + idDoCriterio
+
+const criterioDados = await pesquisarDesejadoPeloIdCriterio(idDoCriterio);
+console.log(criterioDados);
+
 
 buttonAdicionar.addEventListener('click', (event) => {
     event.preventDefault()
@@ -21,7 +29,7 @@ buttonAdicionar.addEventListener('click', (event) => {
 
 })
 
-const createDesejado = async () => {
+const createDesejadoFuncao = async () => {
     const desejado = document.getElementById('desejado')
     const margemMais = document.getElementById('margemMais')
     const margemMenos = document.getElementById('margemMenos')
@@ -29,11 +37,62 @@ const createDesejado = async () => {
     console.log(desejado.value);
     console.log(margemMais.value);
     console.log(margemMenos.value);
+
+    const desejadoCriar = {
+        "valor": desejado.value,
+        "id_criterio": parseInt(idDoCriterio)
+    }
+
+    console.log(desejadoCriar);
+    createDesejado(desejadoCriar)
+
 }
 
 const sendTarefa = document.getElementById('sendTarefa')
 
 sendTarefa.addEventListener('click', (event) => {
     event.preventDefault()
-    createDesejado()
+    createDesejadoFuncao()
 })
+
+const createTableDesejado = () => {
+
+    const containerTable = document.getElementById('tabela')
+
+    criterioDados.forEach((criterio) => {
+
+
+        const resultadoDados = document.createElement('tr')
+        resultadoDados.classList.add('resultadoDados')
+
+        const valorResultado = document.createElement('td')
+        valorResultado.textContent = criterio.valor
+
+        const margem1 = document.createElement('td')
+        const margem2 = document.createElement('td')
+
+        if (criterio.margem_erro) {
+
+            criterio.margem_erro.forEach((margemErro) => {
+
+                if (margemErro.minimo) {
+                    margem1.textContent = margemErro.minimo
+                }
+
+                if (margemErro.maximo) {
+                    margem2.textContent = margemErro.maximo
+                }
+
+            })
+
+
+
+        }
+
+        containerTable.append(resultadoDados)
+        resultadoDados.append(valorResultado, margem1, margem2)
+
+    })
+}
+
+createTableDesejado()
