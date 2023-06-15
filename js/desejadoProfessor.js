f71b1a08d3670da31baffc637c930ea4213681ad
unknown <biancapeleao@gmail.com>
2023-06-15 09:23:13 -0300

    ficou faltando create de desejado

--- js/desejadoProfessor.js
+++ js/desejadoProfessor.js
@@ -14,3 +14,20 @@ buttonAdicionar.addEventListener('click', (event) => {
 
 
 })
+
+const createDesejado = async () => {
+    const desejado = document.getElementById('desejado')
+    const margemMais = document.getElementById('margemMais')
+    const margemMenos = document.getElementById('margemMenos')
+
+    console.log(desejado.value);
+    console.log(margemMais.value);
+    console.log(margemMenos.value);
+}
+
+const sendTarefa = document.getElementById('sendTarefa')
+
+sendTarefa.addEventListener('click', (event) => {
+    event.preventDefault()
+    createDesejado()
+})
